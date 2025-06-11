5d4b9c60a03e5a9cac0a386374bc1261474813a6
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2025-06-11 10:05:45 +1000

    Shorten stack names

--- infrastructure/toolchain/stateless-stack.ts
+++ infrastructure/toolchain/stateless-stack.ts
@@ -9,7 +9,7 @@ export class StatelessStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new DeploymentStackPipeline(this, 'OrcaBusStatelessBclConvertInteropQcStack', {
+    new DeploymentStackPipeline(this, 'StatelessBclConvertInteropQcStack', {
       githubBranch: 'main',
       githubRepo: REPO_NAME,
       stack: StatelessApplicationStack,
