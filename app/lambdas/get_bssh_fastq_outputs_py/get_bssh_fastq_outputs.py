66a33b37cb90dd30f560f6ca06f494a11a79fcf1
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2025-10-25 13:18:21 +1100

    Fix search for bssh workflow for legacy wfm

--- app/lambdas/get_bssh_fastq_outputs_py/get_bssh_fastq_outputs.py
+++ app/lambdas/get_bssh_fastq_outputs_py/get_bssh_fastq_outputs.py
@@ -7,17 +7,37 @@ Given the instrumentRunId, look through the bssh to aws s3 to get the interop an
 
 """
 
-# Imports
+# Standard imports
+from typing import List
 
 # Layer Imports
 from orcabus_api_tools.workflow import (
     list_workflow_runs_by_workflow_name,
-    get_latest_payload_from_workflow_run
+    get_latest_payload_from_workflow_run,
+    get_workflow_request_response_results
 )
+from orcabus_api_tools.workflow.globals import WORKFLOW_RUN_ENDPOINT
+from orcabus_api_tools.workflow.models import WorkflowRunDetail
 
 # Globals
 BSSH_TO_AWS_S3_WORKFLOW_NAME = "bssh-to-aws-s3"
 
+
+def list_workflow_runs_by_workflow_name_legacy(
+        workflow_name: str,
+) -> List[WorkflowRunDetail]:
+    """
+    Use the query name to get workflows from a workflow name
+    :param workflow_name:
+    :return:
+    """
+
+    return get_workflow_request_response_results(
+        WORKFLOW_RUN_ENDPOINT,
+        params={
+            "workflow__workflowName": workflow_name
+        }
+    )
 
 
 def handler(event, context):
@@ -31,6 +51,18 @@ def handler(event, context):
     # Get inputs
     instrument_run_id = event.get("instrumentRunId")
 
+    bssh_workflows_list = list_workflow_runs_by_workflow_name(
+        workflow_name=BSSH_TO_AWS_S3_WORKFLOW_NAME
+    )
+
+    if len(bssh_workflows_list) == 0:
+        bssh_workflows_list = list_workflow_runs_by_workflow_name_legacy(
+            workflow_name=BSSH_TO_AWS_S3_WORKFLOW_NAME
+        )
+
+    if len(bssh_workflows_list) == 0:
+        return {}
+
     # Get the bssh workflow object
     try:
         bssh_workflow = next(filter(
@@ -39,9 +71,7 @@ def handler(event, context):
                 workflow_run_iter_['currentState']['status'] == 'SUCCEEDED'
             ),
             sorted(
-                list_workflow_runs_by_workflow_name(
-                    workflow_name=BSSH_TO_AWS_S3_WORKFLOW_NAME
-                ),
+                bssh_workflows_list,
                 key=lambda workflow_run_iter_: workflow_run_iter_['orcabusId'],
                 reverse=True
             )
